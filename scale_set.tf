2a3b2616f6724673d910d83b8de960855d970272
mMeydan52 <nizamimeydan@gmail.com>
2022-06-13 11:39:11 +0000

    image fix

--- scale_set.tf
+++ scale_set.tf
@@ -41,14 +41,14 @@ resource "azurerm_linux_virtual_machine_scale_set" "ss" {
     public_key = file("~/.ssh/id_rsa.pub")
   }
 */
-  storage_profile_image_reference {
+  source_image_reference {
     publisher = var.canonical_name
     offer     = var.UbuntuServer_name
     sku       = var.image_sku_name
     version   = "latest"
   }
 
-  storage_profile_os_disk {
+  os_disk {
     create_option        = "FromImage"
     storage_account_type = var.Standard_LRS_name
     caching              = "ReadWrite"
