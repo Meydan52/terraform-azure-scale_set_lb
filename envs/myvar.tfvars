4ba80d6d4537a5c9c324043e1d1321ed47ca498d
mMeydan52 <nizamimeydan@gmail.com>
2022-06-13 17:32:00 +0000

    fmt

--- envs/myvar.tfvars
+++ envs/myvar.tfvars
@@ -1,6 +1,6 @@
 rg_name           = "my_test4SS_rg"
 rg_region         = "Central US"
-subnet_id        = ""
+subnet_id         = ""
 ss_name           = "mytestss-12346"
 vm_sku            = "Standard_D2s_v3"
 instance_number   = 1
@@ -9,4 +9,4 @@ admin_password    = "&*$867user34"
 image_source      = "/subscriptions/77333203-07cc-4166-ac0b-b786a47b8423/resourceGroups/StorageRG/providers/Microsoft.Compute/galleries/UbuntuApache1/images/targetUbuntu/versions/0.0.1"
 computer_name     = "mywpvm"
 Standard_LRS_name = "Standard_LRS"
-db_resource_id = ""
+db_resource_id    = ""
--- module/main.tf
+++ module/main.tf
@@ -1,15 +1,15 @@
 module "azure-scale_set" {
   source            = ""
-rg_name           = "my_test4SS_rg"
-rg_region         = "Central US"
-subnet_id        = ""
-ss_name           = "mytestss-12346"
-vm_sku            = "Standard_D2s_v3"
-instance_number   = 1
-admin_user_name   = "adminuser"
-admin_password    = "&*$867user34"
-image_source      = "/subscriptions/77333203-07cc-4166-ac0b-b786a47b8423/resourceGroups/StorageRG/providers/Microsoft.Compute/galleries/UbuntuApache1/images/targetUbuntu/versions/0.0.1"
-computer_name     = "mywpvm"
-Standard_LRS_name = "Standard_LRS"
-db_resource_id = ""
+  rg_name           = "my_test4SS_rg"
+  rg_region         = "Central US"
+  subnet_id         = ""
+  ss_name           = "mytestss-12346"
+  vm_sku            = "Standard_D2s_v3"
+  instance_number   = 1
+  admin_user_name   = "adminuser"
+  admin_password    = "&*$867user34"
+  image_source      = "/subscriptions/77333203-07cc-4166-ac0b-b786a47b8423/resourceGroups/StorageRG/providers/Microsoft.Compute/galleries/UbuntuApache1/images/targetUbuntu/versions/0.0.1"
+  computer_name     = "mywpvm"
+  Standard_LRS_name = "Standard_LRS"
+  db_resource_id    = ""
 }
